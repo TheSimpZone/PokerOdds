3ef00ab4f36d4e5df4d0aebc6c2c2b1f7b1a298c
Oscar <Oscar@Oscar-PC>
2017-09-20 13:30:59 +0100

    Added scripts.

---
+++ src/Recognition/recognition.py
@@ -0,0 +1,16 @@
+import os
+from VideoCapture import Device
+import PIL
+from PIL import Image
+prefix = 'card'
+increasing = 9
+mainimagename = prefix + str(increasing) +'.jpg'
+inde = input()
+while inde != 'q':
+    Device().saveSnapshot(mainimagename)
+    img = Image.open(mainimagename).resize((640,480),Image.BILINEAR)
+    img.save(mainimagename)
+    img.close()
+    increasing+=1
+    mainimagename = prefix + str(increasing) +'.jpg'
+    inde = input()
